ce44a848250fe61f96c5f9f1183d0727429b5e14
govindjeevan <govindjeevan7@gmail.com>
2017-12-13 10:47:55 +0530

    server.js update

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var bodyParser= require('body-parser')
+var app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+var port = process.env.PORT || 8080
+
+var router= express.Router()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port)
